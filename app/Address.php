5741718e073feb74657086daa056d4c835cd759b
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-12 13:19:05 +0300

    Inverse the relationship

--- app/Address.php
+++ app/Address.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected $fillable = ['user_id', 'country', 'zip'];
+
+    public function user()
+    {
+    	return $this->hasOne('App\User');
+    }
 }
