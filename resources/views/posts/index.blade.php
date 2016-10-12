eedf335c1585f099dd1d452af3e2c268456187cc
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-12 14:44:14 +0300

    Link to a user page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
 					<div class="col-xs-12 col-sm-3 card">
 						<h3><a href="{{ url('posts/' . $post->slug) }}">{{ $post->title }}</a></h3>
 						<p>{{ str_limit($post->content, 100) }} ...</p>
-						<p>Author <em>{{ $post->user->name }}</em></p>
+						<p>Author <a href="{{ url('members/' . $post->user->id) }}"><em>{{ $post->user->name }}</em></a></p>
 						<p><em>{{ $post->created_at->diffForHumans() }}</em></p>
 					</div>
 				@endforeach
